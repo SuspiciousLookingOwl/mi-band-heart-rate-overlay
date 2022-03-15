bf6a0493351964d5c5340ef981ce2ea9040eeb34
SuspiciousLookingOwl <vinjongm@gmail.com>
2022-03-16 00:28:57 +0700

    add timestamp

--- main.go
+++ main.go
@@ -9,33 +9,42 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var latestHeartRate int = 0
-
-// For logging
-// var f, _ = os.OpenFile("heart_rate.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-// f.Write([]byte(timestamp + "," + strconv.Itoa(payload.HeartRate) + "\n"))
+type HeartRateData struct {
+	Timestamp time.Time `json:"timestamp"`
+	HeartRate int       `json:"heartRate"`
+}
 
 type Payload struct {
 	HeartRate int `json:"heartRate"`
 }
 
+var latestHeartRateData HeartRateData = HeartRateData{
+	HeartRate: 0,
+	Timestamp: time.Now(),
+}
+
+// For logging
+// var f, _ = os.OpenFile("heart_rate.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+// f.Write([]byte(timestamp + "," + strconv.Itoa(payload.HeartRate) + "\n"))
+
 func logHeartRate(c echo.Context) error {
-	payload := new(Payload)
+	payload := new(HeartRateData)
 	if err := c.Bind(payload); err != nil {
 		return err
 	}
 
-	timestamp := time.Now().Format(time.RFC3339)
-	latestHeartRate = payload.HeartRate
-	fmt.Printf("[%s] New Heart Rate %d\n", timestamp, payload.HeartRate)
+	timestamp := time.Now()
+	latestHeartRateData = HeartRateData{
+		Timestamp: timestamp,
+		HeartRate: payload.HeartRate,
+	}
+	fmt.Printf("[%s] New Heart Rate %d\n", timestamp.Format(time.RFC3339), payload.HeartRate)
 
 	return c.String(http.StatusOK, "")
 }
 
 func retrieveHeartRate(c echo.Context) error {
-	return c.JSON(http.StatusOK, Payload{
-		HeartRate: latestHeartRate,
-	})
+	return c.JSON(http.StatusOK, latestHeartRateData)
 }
 
 func main() {
